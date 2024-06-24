957a330fc757b1e9d31c4cbc07e34b0c552e43ea
Maxi <mpb71677@gmail.com>
2024-06-23 23:30:27 -0300

    Terminado

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/inicio')
+def inicio():
+    return render_template('inicio.html')
+
+@app.route('/informacion')
+def informacion():
+    return render_template('informacion.html')
+
+@app.route('/ingresado')
+def ingresado():
+    return render_template('ingresado.html')
+
+@app.route('/sponsors')
+def sponsors():
+    return render_template('sponsors.html')
+
+@app.route('/deportes')
+def deportes():
+    return render_template('deportes.html')
+
+@app.route('/calendario')
+def calendario():
+    return render_template('calendario.html')
+
+@app.route('/precios')
+def precios():
+    return render_template('precios.html')
+
+if __name__ == '__main__':
+    app.run(debug=True,  port=3500) 
